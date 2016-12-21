c5f12f2deab79870b8778ab35cb778cd44f72cf8
JarradBlumer <jarrad.blumer@gmail.com>
2016-12-20 22:42:43 -0600

    Asset adjustment
    Got rid of the 2D Standard Assets from unity, and added in some of my
    own assets. The Grid and a monster so far.

---
+++ Assets/Scripts/MonsterBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterBehavior : MonoBehaviour {
+
+    public Transform MonsterTransform;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Testing monster movement horizontal
+        MonsterTransform.position = new Vector2(MonsterTransform.position.x + 0.008f, MonsterTransform.position.y); // Moves the monster on the x-axis every update. y position stays the same
+	}
+}
