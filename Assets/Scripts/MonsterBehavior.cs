ad2d7c4c28b2faf6d30e06a2814c6d56817dce98
JarradBlumer <jarrad.blumer@gmail.com>
2016-12-22 00:13:26 -0600

    Added random numbers
    Numbers now populate the 'grid'. Their value is random between 0 - 99.
    The monster now checks to see if it collides with a number. Will
    eventually check to see if the number is a correct answer and 'munch' it
    if true.

--- Assets/Scripts/MonsterBehavior.cs
+++ Assets/Scripts/MonsterBehavior.cs
@@ -13,38 +13,55 @@ public class MonsterBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        //Testing monster movement horizontal
+        
         
 	}
 
+    /// <summary>
+    /// Currently randomly moves the monster randomly in a direction on the 'grid'.  Will eventually have some basic AI to hunt either player, or correct answers.
+    /// </summary>
+    /// <returns></returns>
     IEnumerator MoveMonster()
     {
         //int random = Random.Range(0, 3); // 0 - left, 1 - up, 2 - right, 3 - down
 
         while (true)
         {
-            int random = Random.Range(0, 4); // 0 - left, 1 - up, 2 - right, 3 - down
+            int random = Random.Range(0, 4); // 0 - left, 1 - up, 2 - right, 3 - down   Random.Range(int, int) is [inclusive, exclusive). so our range of 0-4 returns 0, 1, 2, 3
 
             if (random == 0 && MonsterTransform.position.x < 2) // right
             {
-                MonsterTransform.position = new Vector2(MonsterTransform.position.x + 1, MonsterTransform.position.y); // Moves the monster on the x-axis every update. +1 to move right, -1 to move left. y position stays the same
-                yield return new WaitForSeconds(4);
+                MonsterTransform.position = new Vector2(MonsterTransform.position.x + 1, MonsterTransform.position.y); // Moves the monster on the x-axis. +1 to move right, -1 to move left. y position stays the same
+                yield return new WaitForSeconds(2);
             }
             if (random == 2 && MonsterTransform.position.x > -2) // left
             {
                 MonsterTransform.position = new Vector2(MonsterTransform.position.x - 1, MonsterTransform.position.y); 
-                yield return new WaitForSeconds(4);
+                yield return new WaitForSeconds(2);
             }
             if (random == 1 && MonsterTransform.position.y < 2) // up
             {
-                MonsterTransform.position = new Vector2(MonsterTransform.position.x, MonsterTransform.position.y + 1); 
-                yield return new WaitForSeconds(4);
+                MonsterTransform.position = new Vector2(MonsterTransform.position.x, MonsterTransform.position.y + 1); // Moves the monster on the y-axis. +1 up, -1 down.
+                yield return new WaitForSeconds(2);
             }
             if (random == 3 && MonsterTransform.position.y > -2) // down
             {
                 MonsterTransform.position = new Vector2(MonsterTransform.position.x, MonsterTransform.position.y - 1); 
-                yield return new WaitForSeconds(4);
+                yield return new WaitForSeconds(2);
             }
+        }
+    }
+
+    /// <summary>
+    /// Checks to see if the monster runs into a number.  Will also check to see if the number is a correct value and 'munch' it if it is.
+    /// </summary>
+    /// <param name="collider">gets information about the gameobject that triggered</param>
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Number")
+        {
+            Debug.Log(gameObject.name + "ran into a number!");
+
         }
     }
 }
---
+++ Assets/Scripts/NumberManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberManager : MonoBehaviour {
+
+    public TextMesh txtMesh;                // Get the mesh prefab
+
+	void Awake ()
+    {
+        // Initialize the board of numbers
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            { 
+                Instantiate(txtMesh, new Vector2(i - 2, j - 2), Quaternion.identity); // i and j values - 2 to line up with my grid since the middle is 0,0.  which means my 5x5 grid goes from -2 to 2 x/y
+                txtMesh.text = Random.Range(0, 100).ToString(); // Fills each number with a value between 0 and 99 (inclusive, exclusive)
+            }
+        }
+	}
+	
+	void Start ()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+}
